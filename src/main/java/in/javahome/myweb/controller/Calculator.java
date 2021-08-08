13855ec858e8f7e9ded4642fcd10a052ff1046b6
Thang Ngo <thang.ngo@hitachivantara.com>
2021-08-08 22:08:21 +0700

    first commit

---
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -0,0 +1,24 @@
+package in.javahome.myweb.controller;
+/*
+ * 
+ */
+public class Calculator {
+	
+	/*
+	 * @param i
+	 * @param j
+	 * @return int
+	 */
+	public int add(int i, int j){
+		String name = null;
+		name.toUpperCase();
+		return i+j;
+	}
+	public int multiply(int i, int j){
+		return i*j;
+	}
+	
+	public int sub(int i, int j){
+		return i-j;
+	}
+}
